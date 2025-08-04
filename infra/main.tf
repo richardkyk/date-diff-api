8d7b88088c070d4cb88eca9909adfc8e4f880949
richardkyk <28803078+richardkyk@users.noreply.github.com>
2025-08-04 17:35:30 +1000

    refactor: use API gateway instead of invoking lambda directly

--- infra/main.tf
+++ infra/main.tf
@@ -70,11 +70,39 @@ resource "aws_lambda_function" "lambda" {
 
 }
 
-resource "aws_lambda_function_url" "default" {
-  function_name      = aws_lambda_function.lambda.function_name
-  authorization_type = "NONE" # Or "AWS_IAM" for secured access
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "${var.app_name}-http-api"
+  protocol_type = "HTTP"
 }
 
-output "lambda_url" {
-  value = aws_lambda_function_url.default.function_url
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.lambda.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_lambda_permission" "api_gw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "ANY /{proxy+}" # catch all
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_stage.default_stage.invoke_url
 }
